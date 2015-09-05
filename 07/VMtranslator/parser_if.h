aedf0599fe3937707bf07930c4e506326aa64f04
Sam-Z <765350744@qq.com>
2015-09-05 16:39:46 +0800

    change style to linux

---
+++ 07/VMtranslator/parser_if.h
@@ -0,0 +1,16 @@
+#ifndef __PARSER_IF_H
+#define __PARSER_IF_H
+#include<stdio.h>
+
+char * parser_get_line(FILE fd);
+
+
+
+
+
+
+
+
+
+
+#endif
---
+++ 07/VMtranslator/parser_tc.h
@@ -0,0 +1,15 @@
+#ifndef __PARSER_TC_H
+#define __PARSER_TC_H
+#include<stdio.h>
+
+
+
+
+
+
+
+
+
+
+
+#endif
